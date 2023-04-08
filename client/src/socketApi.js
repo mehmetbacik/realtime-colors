943b2190d017161ccd09652b1f1c1f6d23adc29b
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-04-09 01:36:07 +0300

    Update
    Updated for vercel.

--- client/src/socketApi.js
+++ client/src/socketApi.js
@@ -5,13 +5,9 @@ let socket;
 export const init = () => {
 	console.log("Sunucuya bağlanılıyor...");
 
-	socket = io("https://realtime-colors-teo6.vercel.app/", {
+	socket = io("http://localhost:3001", {
 		transports: ["websocket"],
 	});
-
-	/*socket = io("http://localhost:3001", {
-		transports: ["websocket"],
-	});*/
 
 	socket.on("connect", () =>
 		console.log("Sunucuya bağlantı başarıyla gerçekleşti.")
