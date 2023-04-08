8bcc94eeb993250e92753e5a7772231fd60c82ac
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-04-09 01:23:18 +0300

    Update
    Implemented tailwindcss and updated palette.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import {useEffect} from 'react';
-import {init} from './socketApi';
+import {useEffect, useState} from 'react';
+import {init, subscribe} from './socketApi';
 import Palette from './components/Palette';
 
 function App() {
+  const [activeColor, setActiveColor] = useState('#282c34');
+
   useEffect (() => {
     init();
+    subscribe((color) => {
+      setActiveColor(color);
+    });
   }, [])
 
   return (
-    <div className="App">
-      <Palette />
+    <div className="App flex flex-col items-center justify-center" style={{backgroundColor: activeColor}}>
+      <div className='content__text'>
+        <h1>{activeColor}</h1>
+      </div>
+      <Palette activeColor={activeColor} />
     </div>
   );
 }
--- client/src/components/Palette.js
+++ client/src/components/Palette.js
@@ -1,12 +1,13 @@
-import React from "react"
-
-function Palette() {
-  return (
-    <div className="palette">
-        <input type="color" />
-        <button>Click</button>
-    </div>
-  );
+import {useState} from "react";
+import {send} from '../socketApi';
+function Palette({activeColor}) {
+    const [color, setColor] = useState('#000');
+    return (
+        <div className="palette flex flex-col items-center justify-center gap-4 bg-white p-8 rounded-xl">
+            <input type="color" value={activeColor} onChange={(e) => setColor(e.target.value)}/>
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => send(color)}>Click</button>
+        </div>
+    );
 }
 
 export default Palette;
